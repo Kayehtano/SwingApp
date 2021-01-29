28bd6977254d1a13cdecbf58a1b158ba59f248d5
Enzo Cayetano <91093673@g.mvusd.net>
2021-01-29 20:29:21 +0000

    Added two more buttons

--- Main.java
+++ Main.java
@@ -10,7 +10,38 @@ public class Main
 		// Must have this line
 		frame1.setSize(500, 500);
 
-		
+		// Buttons
+		JButton submit = new JButton("Submit");
+		JButton remove = new JButton("Remove");
+		JButton clear = new JButton("Clear");
+
+		submit.addActionListener(new ActionListener()
+		{
+			public void actionPerformed(ActionEvent e)
+			{
+				System.out.println("Submitted.");
+			}
+		}); // end of action listener
+
+		remove.addActionListener(new ActionListener()
+		{
+			public void actionPerformed(ActionEvent e)
+			{
+				System.out.println("Removed.");
+			}
+		}); // end of action listener
+
+		clear.addActionListener(new ActionListener()
+		{
+			public void actionPerformed(ActionEvent e)
+			{
+				System.out.println("Cleared.");
+			}
+		}); // end of action listener
+
+		frame1.add(submit);
+		frame1.add(remove);
+		frame1.add(clear);
 
 		frame1.setVisible(true);
 	} // main end
